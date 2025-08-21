1c7e694e3fe359c5baa355793ed66a59662361e5
serr <sc7227484@gmail.com>
2025-08-21 17:02:07 +0300

    quick update

--- test.cpp
+++ test.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 
-int global_num;
-
 int main() {
-    int local_num;
-    std::cout << global_num << " " << local_num << std::endl;
+    std::cout << "beep at the end of message\a\n";
 }
