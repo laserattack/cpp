a7484222644dd7e6360bf9df1e195fdea620ef53
serr <sc7227484@gmail.com>
2025-08-21 13:12:41 +0300

    quick update

--- base/functions.cpp
+++ base/functions.cpp
@@ -2,7 +2,7 @@
 
 void ex_lambda() {
     const char* msg = "hello, sailor!";
-    // & => всё из родительской области видимост 
+    // & => всё из родительской области видимости 
     // захватывает по ссылке (reference)
     auto print = [&](int number) { 
         std::cout << number << msg << std::endl; 
